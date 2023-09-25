23803f0ff70a8d547a2cb9bcac7f8d6f74b2e1d9
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-09-25 20:19:06 +0530

    selection sort

--- Main.java
+++ Main.java
@@ -1,20 +1,34 @@
-import java.math.BigInteger;
 
+import java.util.*;
 public class Main {
-	public static BigInteger findGCD(int A, String B) {
-		// Convert string B to a BigInteger
-		BigInteger bigB = new BigInteger(B);
-		// Convert int A to a BigInteger
-		BigInteger bigA = BigInteger.valueOf(A);
-		// Find the greatest common divisor of A and B using the gcd method from the BigInteger class
-		BigInteger result = bigA.gcd(bigB);
-		return result;
-	}
+	public void sort(int arr[]) {
 
-	public static void main(String[] args) {
-		// Example
-		int A = 10;
-		String B = "128769586950601";
-		System.out.println(findGCD(A, B)); // Output: 10
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			int min = i;
+			for (int j = i + 1; j < n; j++) {
+				if (arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+			int temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+		}
+
+	}
+	public void printArr(int arr[]){
+		for(int i = 0 ; i<arr.length; i++){
+			System.out.print(arr[i]+" ");
+		}
+	}
+	public static void main(String[] args){
+		int[] arr  = {84,54,6,56,7,8,6,8,5,67,5,567};
+		Main ob = new Main();
+		ob.sort(arr);
+		ob.printArr(arr);
 	}
 }
+
+
