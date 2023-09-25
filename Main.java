23594e83eaf0743464c16e1d386db4b478ce86ad
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-09-25 20:24:30 +0530

    bubble sort

--- Main.java
+++ Main.java
@@ -5,17 +5,15 @@ public class Main {
 
 		int n = arr.length;
 
-		for (int i = 0; i < n; i++) {
-			int min = i;
-			for (int j = i + 1; j < n; j++) {
-				if (arr[j] < arr[min]) {
-					min = j;
-				}
+	for(int i = n-1;i>1;i--){
+		for(int j = 0;j<i-1;j++){
+			if(arr[j]>arr[j+1]) {
+				int temp = arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
 			}
-			int temp = arr[min];
-			arr[min] = arr[i];
-			arr[i] = temp;
 		}
+	}
 
 	}
 	public void printArr(int arr[]){
